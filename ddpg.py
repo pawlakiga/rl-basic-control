751e49d55520f249c290b9120208a88406c6bb92
Iga Pawlak <36933957+pawlakiga@users.noreply.github.com>
2024-01-19 17:00:20 +0100

    Add files via upload

--- ddpg.py
+++ ddpg.py
@@ -4,7 +4,12 @@ import torch.optim as optim
 import torch.nn as nn
 from networks import *
 from ddpg_utils import *
+from environment import *
+import pickle
+import os 
+import tqdm
 
+# Definition of the agent class
 class DDPGAgent:
     def __init__(self, env, hidden_size=256, actor_learning_rate=1e-4, critic_learning_rate=1e-3, gamma=0.99, tau=1e-2, max_memory_size=50000):
         # Params
@@ -69,3 +74,140 @@ class DDPGAgent:
        
         for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
             target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
+
+    def save(self, models_path) :
+        if not os.path.isdir(models_path):
+            os.makedirs(models_path)
+        torch.save(self.actor, os.path.join(models_path, 'actor.pth'))
+        torch.save(self.actor_target, os.path.join(models_path,'actor-target.pth'))
+        torch.save(self.critic, os.path.join(models_path,'critic.pth'))
+        torch.save(self.critic_target, os.path.join(models_path,'critic-target.pth'))
+
+###############################################################################################################################################
+############################## Functions for training and testing the agent ###################################################################
+        
+
+def train_agent(m, max_episode_len, Ki, n_episodes):
+    print(f"In train agent")
+    env = BasicEnvironment(m = m, max_episode_len = max_episode_len, Ki = Ki)
+    print(f"Initialised environment")
+    agent = DDPGAgent(env)
+    print(f"Initialised agent")
+    batch_size = 128
+    rewards = []
+    avg_rewards = []
+    last_rewards = []
+    episodes = tqdm.trange(n_episodes, desc = 'Training', leave = True)
+
+    for episode in episodes:
+        
+        state, done = env.reset()
+        state = np.array(state, dtype=float)
+        episode_reward = 0
+        t = 0
+        while not done:
+            action = agent.get_action(state)
+            # print(f"At timestep {t} action is {action}\r")
+            new_state, reward, done = env.step(action) 
+            if done : 
+                break
+            new_state = np.array(new_state, dtype=float)
+            action = np.array([action], dtype=float)
+            reward = np.array(reward, dtype = float)
+            agent.memory.append((state, action, reward, new_state, done))
+            agent.memory.append((state, action, reward, new_state, done))
+            
+            if len(agent.memory) > batch_size:
+                agent.update(batch_size)        
+            
+            state = new_state
+            episode_reward += reward[0]
+            t+= 1
+
+        last_rewards.append(reward[0])
+        rewards.append(episode_reward)
+        avg_rewards.append(np.mean(rewards[-10:]))
+        episodes.set_description("episode: {} terminated after {} steps, last state {:4f}, reward: {}, average reward: {:2f}".format(episode,t, state[0], np.round(episode_reward, decimals=2), np.mean(rewards[-5:])))
+
+        # print("episode: {} terminated after {} steps, last state {}, reward: {}, average reward: {}".format(episode,t, state[0], np.round(episode_reward, decimals=2), np.mean(rewards[-5:])))
+        
+        
+    plt.plot(rewards)
+    plt.plot(avg_rewards)
+    plt.plot(last_rewards)
+    plt.xlabel('Episode')
+    plt.ylabel('Reward')
+    plt.legend(['episode rewards', 'average episode rewards', 'last rewards in episode'])
+    plt.grid()
+    plt.show()
+    return agent
+
+def test_agent(m, delay, agent): 
+    delayed_env = DelayedEnvironment(m = m, start_state=0.0, delay=delay, max_episode_len=30*(delay+1))
+    ######################## Simulation of the agent #####################################
+    states = []
+    rewards = []
+    cum_rewards = []
+    actions = []
+    state, done = delayed_env.reset()
+    i = 0
+    while not done: 
+        action = agent.get_action(state)
+        # if delayed_env.timestep < delayed_env.delay : 
+        #     actions.append(0)
+        # else : 
+        #     actions.append(delayed_env.actions_queue[0])
+        next_state, reward, done = delayed_env.step(action=action)
+        if done : 
+            print(f"Terminated at timestep {i} with current: {delayed_env.cur_state} and next: {next_state} and desired : {delayed_env.desired_state}")
+            break
+        states.append(state)
+        rewards.append(reward)
+        actions.append(action)
+        if i == 0 : 
+            cum_rewards.append(reward)
+        else: 
+            cum_rewards.append(cum_rewards[-1] + reward)
+        i+=1
+        state = next_state
+    ######################## Simulation with a step function #############################
+    states_model = []
+    rewards_model = []
+    cum_rewards_model = []
+    state, done = delayed_env.reset()
+    i = 0
+    while not done: 
+        action = 0.8
+        next_state, reward, done = delayed_env.step(action=action)
+        if done : 
+            break
+        states_model.append(state)
+        rewards_model.append(reward)
+        if i == 0 : 
+            cum_rewards_model.append(reward)
+        else: 
+            cum_rewards_model.append(cum_rewards[-1] + reward)
+        i+=1
+        state = next_state
+
+    f, ax = plt.subplots(1,3,figsize = (20,3))   
+    ax[0].plot(states)
+    ax[0].plot(states_model)
+    ax[0].axhline(y = delayed_env.desired_state, xmax=len(states)-1, color = 'green', linestyle = '--')
+    ax[0].grid()
+    ax[0].legend(['agent', 'model','desired state'])
+    ax[0].set_xlabel('time')
+    ax[0].set_ylabel('state')
+    ax[1].plot(rewards)
+    ax[1].plot(rewards_model)
+    ax[1].legend(['agent', 'model'])
+    ax[1].set_xlabel('time')
+    ax[1].set_ylabel('step reward')
+    ax[1].grid()
+    ax[2].plot(actions)
+    ax[2].legend(['agent selected action'])
+    ax[2].axhline(y = delayed_env.desired_state, xmax=len(states_model)-1, color = 'orange')
+    ax[2].grid()
+    plt.show()
+
+    return states, states_model, rewards, rewards_model, actions
--- environment.py
+++ environment.py
@@ -1,11 +1,12 @@
 from typing import Any, SupportsFloat
 import numpy as np
 import gymnasium as gym 
+import torch
 
 class BasicEnvironment(gym.Env): 
     metadata = {"render_modes:" : []}
 
-    def __init__(self, m = 1, d = 1, start_state = None, desired_state = 0.8, Ts = 0.1, render_mode = None, thresholds = [0.001, 0.001], max_episode_len = 30) -> None:
+    def __init__(self, m = 1, d = 1, start_state = None, desired_state = 0.8, Ts = 0.1, render_mode = None, thresholds = [0.001, 0.001], max_episode_len = 30, Ki = 0) -> None:
         super().__init__()
         self.d = d 
         self.m = m
@@ -13,14 +14,16 @@ class BasicEnvironment(gym.Env):
         self.cur_state = start_state
         self.desired_state = desired_state
         self.Ts = Ts
-        self.observation_space = gym.spaces.Box(low = 0, high = np.inf, dtype = float)
+        self.observation_space = gym.spaces.Box(low = 0, high = 10, dtype = float)
         self.action_space = gym.spaces.Box(low = 0, high = 1, dtype = float)
         self.render_mode = render_mode
         self.timestep = 0
         self.max_episode_len = max_episode_len
         self.thresholds = thresholds
         self.last_reward = 0
+        self.int_reward = 0
         self.rewards_history = []
+        self.Ki = Ki
 
 
     def reset(self, seed = None):
@@ -30,12 +33,13 @@ class BasicEnvironment(gym.Env):
         else: 
             self.cur_state = np.array([self.start_state])
         self.timestep = 0
-        self.actions_queue = []
         self.last_reward = 0
+        self.int_reward = 0
         self.rewards_history = []
         return self.cur_state, False
 
     def step(self, action: Any) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
+        action = np.clip(action, self.action_space.low, self.action_space.high)
         new_state = self.cur_state * (1 - self.d * self.Ts/self.m) + self.Ts / self.m * action
         self.timestep += 1
         done_state = self.done(new_state=new_state)
@@ -46,23 +50,19 @@ class BasicEnvironment(gym.Env):
         return self.cur_state, self.last_reward, done_state
 
     def done(self, new_state): 
-        # print(f"(np.abs(new_state - self.desired_state) <= self.thresholds[0]):  {(np.abs(new_state - self.desired_state) <= self.thresholds[0])}")
-        # print(f"np.abs(new_state - self.cur_state) <= self.thresholds[1]: {np.abs(new_state - self.cur_state) <= self.thresholds[1]}")
-        # print(f"self.timestep > self.max_episode_len: {self.timestep > self.max_episode_len}")
-        return ((np.abs(new_state - self.desired_state) <= self.thresholds[0]) and (np.abs(new_state - self.cur_state) <= self.thresholds[1])) or self.timestep > self.max_episode_len
+        return self.timestep > self.max_episode_len
+        # return ((np.abs(new_state - self.desired_state) <= self.thresholds[0]) and (np.abs(new_state - self.cur_state) <= self.thresholds[1])) or self.timestep > self.max_episode_len
     
     def reward_fun(self, action): 
-        step_reward = - np.abs(self.desired_state  - self.cur_state)  
-        reward = np.sum(self.rewards_history) * self.Ts * 0.8 + step_reward
-        # print(f"At timestep {self.timestep} step reward is {step_reward} and total reward {reward}")
-        self.rewards_history.append(step_reward)
-        return reward
+        step_reward = - np.abs(self.desired_state  - self.cur_state) 
+        self.int_reward += step_reward * self.Ts
+        return step_reward + self.int_reward * self.Ki
         # return - np.abs(self.desired_state  - self.cur_state)  
       
     def close(self):
         return super().close()
         
-class DelayedEnvironment(BasicEnvironment): 
+class DelayedEnvironment(BasicEnvironment):
     def __init__(self, m=1, d=1, start_state=None, desired_state=0.8, Ts=0.1, render_mode=None, thresholds=[0.001, 0.001], max_episode_len=30, delay = 0) -> None:
         super().__init__(m, d, start_state, desired_state, Ts, render_mode, thresholds, max_episode_len)
         self.delay = delay
@@ -81,6 +81,10 @@ class DelayedEnvironment(BasicEnvironment):
                 self.actions_queue.append(action)
         # print(f"At timestep {self.timestep} actions queue {self.actions_queue}")
         return super().step(delayed_action)
+    
+    def reset(self):
+        self.actions_queue = []
+        return super().reset()    
 
 
 
